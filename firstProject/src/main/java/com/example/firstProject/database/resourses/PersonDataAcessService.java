04e3f4a91ef3d59aa1c9a04d29b129ed2f068feb
Mohamed Ezzat <52044869+mohamedezzat999@users.noreply.github.com>
2022-08-01 10:44:20 +0200

    lastupdate

--- firstProject/src/main/java/com/example/firstProject/database/resourses/PersonDataAcessService.java
+++ firstProject/src/main/java/com/example/firstProject/database/resourses/PersonDataAcessService.java
@@ -2,29 +2,59 @@ package com.example.firstProject.database.resourses;
 
 import com.example.firstProject.database.daos.PersonDao;
 import com.example.firstProject.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 @Repository("postgres")
 public class PersonDataAcessService implements PersonDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
     private static List<Person> db =new ArrayList<>();
+    @Autowired
+    public PersonDataAcessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int instertPerson(UUID id, Person person) {
+
+        final String sql = "INSERT  INTO person (id,name) VALUES (? , ?)";
+
+
         db.add(new Person(id , person.getName()));
         return 1;
     }
 
     @Override
     public List<Person> selectAllPeople() {
-        return db;
+        final String sql = "SELECT  id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) ->{
+            UUID id =UUID.fromString(resultSet.getString("id"));
+            String name  = resultSet.getString("name");
+            return new Person(id,name);
+        });
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return db.stream().filter(person -> person.getId().equals(id)).findFirst();
+        final String sql = "SELECT  id, name FROM person WHERE id = ?";
+
+        Person person = jdbcTemplate.queryForObject(
+                sql,
+                new Object[]{id},
+                (resultSet, i) ->{
+                    UUID personId =UUID.fromString(resultSet.getString("id"));
+                    String name  = resultSet.getString("name");
+                    return new Person(personId,name);
+                });
+        return Optional.ofNullable(person);
     }
 
     @Override
